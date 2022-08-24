08b57d27c9b28ce429dd1ab075d1f427e4a762b9
Rosemary <rjingrey@gmail.com>
2022-08-24 13:02:24 +1000

    reverseString.spec.js tests passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(strIn) {
+    let strArray = strIn.split('');
+    let strOut = strArray.reverse()
+    return strOut.join('').toString();
 };
 
 // Do not edit below this line
