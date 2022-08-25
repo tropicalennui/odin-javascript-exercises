42686e0965e8bec6db962b4bfc92a553360c9ee7
Rosemary <rjingrey@gmail.com>
2022-08-25 21:01:27 +1000

    tempConversion.spec.js tests passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,25 @@
-const ftoc = function() {
+const ftoc = function(f) {
+  
+  // [°C] = ([°F] − 32) × 5⁄9
 
-};
+  let c = (f-32) * (5/9);
+  if (c%1 != 0) {
+    c = c.toFixed(1);
+  }
 
-const ctof = function() {
+  return Number.parseFloat(c);
+
+}
+
+const ctof = function(c) {
+// [°F] = [°C] × 9⁄5 + 32	
+
+  let f = c * 9/5 + 32;
+  if (f%1 != 0) {
+    f = f.toFixed(1);
+  }
+
+  return Number.parseFloat(f);
 
 };
 
