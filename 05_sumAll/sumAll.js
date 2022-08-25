29900c0e20b7bc1eca4db542f7845e94abbad9ee
Rosemary <rjingrey@gmail.com>
2022-08-25 17:39:09 +1000

    tests in sumAll.spec.js all passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    if (num1 < 0 || num2 < 0 || typeof num1 != 'number' || typeof num2 != 'number') {
+        return 'ERROR';
+    } else {
+    
+        const args = Array.from(arguments);
+        args.sort((a,b) => a - b);
+
+        let buildValues = [args[0]];
+        let count = args[0];
+
+        for (i=1; i <= args[1]-args[0]; i++) {
+            count++;
+            buildValues.push(count);
+        }
+
+        let output = buildValues.reduce((accumulator, value) => {
+            return accumulator + value;
+        }, 0);
+
+
+        return output;
+    
+    }
 };
 
 // Do not edit below this line
