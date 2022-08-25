fa5f7732270721e4ea0d2238a0c6b28e06a69003
Rosemary <rjingrey@gmail.com>
2022-08-25 17:56:36 +1000

    leapYears.spec.js tests passed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    let tests = [];
+    
+    tests.push(year%4, year%100, year%400);
+
+    if ((tests[0] == 0 && tests[1] != 0) || (tests[0] == 0 && tests[1] == 0 && tests[2] == 0)) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
