1d4078cbe1cc8cb9efbef5e4b5c8de3737fbe2e5
Rosemary <rjingrey@gmail.com>
2022-08-25 13:02:16 +1000

    all removeFromArray.sec.js tests passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,3 +1,20 @@
+//removes multiple values
+
+let output = '';
+
+const removeFromArray = function() {
+
+    const args = Array.from(arguments);
+    const input = args[0];
+    const remove = new Set(args.slice(1));
+    
+    output = input.filter(x => !remove.has(x));
+
+    return output;
+
+};
+
+/*
 //removes a single value
 let output = '';
 
@@ -6,9 +23,13 @@ const removeFromArray = function(input, remove) {
     remove = remove.toString();
 
     output = input.filter(content => content != remove);
+
     return output;
 
 };
+*/
+
+//removes multiple values
 
 // Do not edit below this line
 module.exports = removeFromArray;
