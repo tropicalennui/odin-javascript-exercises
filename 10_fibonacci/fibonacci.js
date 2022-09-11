a8febe6ffb9bce5958d5b4380a92be546bcf9177
Rosemary <rjingrey@gmail.com>
2022-09-11 10:43:50 +1000

    tests passed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+	if (num < 0) {
+		return 'OOPS';
+	}
+	let skipZero = num + 1;
+	let sequence = Array.from({ length: skipZero }).reduce(
+		(acc, val, i) => acc.concat(i > 1 ? acc[i - 1] + acc[i - 2] : i), []
+	);
+	return sequence[num];
 };
 
 // Do not edit below this line
