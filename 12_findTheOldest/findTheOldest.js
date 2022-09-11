86a40e856c58558b12f26869f00d01b9cdb5c7ee
Rosemary <rjingrey@gmail.com>
2022-09-12 06:58:33 +1000

    tests passed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+
+	const currentYear = new Date().getFullYear();
+
+	let result = array.reduce((a, b) => {
+		if (a.yearOfDeath == null) {
+			a.yearOfDeath = currentYear;
+		}
+		if (b.yearOfDeath == null) {
+			b.yearOfDeath = currentYear;
+		}
+		return a.yearOfDeath - a.yearOfBirth < b.yearOfDeath - b.yearOfBirth ? b : a;
+	});
+
+	return result;
 
 };
 
